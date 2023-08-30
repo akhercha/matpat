330390d4fd9533da5420339fa494fc5d3f717095
akhercha <adelkherchache@gmail.com>
2023-08-30 13:33:46 +0200

    done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+fn glob(pattern: &str, text: &str) -> bool {
+    let mut pattern = pattern.chars().peekable();
+    let mut text = text.chars().peekable();
+
+    while pattern.peek().is_some() && text.peek().is_some() {
+        match pattern.peek().unwrap() {
+            '*' => {
+                text.next();
+                pattern.next();
+            },
+            '?' => panic!("Not implemented yet"),
+            '[' => panic!("Not implemented yet"),
+            _ => {
+                if pattern.peek() != text.peek() {
+                    return false;
+                }
+                text.next();
+                pattern.next();
+            },
+        }
+    }
+    return pattern.peek().is_none() && text.peek().is_none();
+}
+
 fn main() {
-    println!("Hello, world!");
+    let res: bool = glob("Hello", "*ello");
+    println!("{:?}", res);
 }
