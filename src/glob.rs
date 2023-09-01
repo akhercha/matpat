aca349f07eb7965303b8347ec1f4c97d6d494932
akhercha <adelkherchache@gmail.com>
2023-09-01 21:40:06 +0200

    main(main): fixed bug

---
+++ src/glob.rs
@@ -0,0 +1,122 @@
+use super::glob_result::GlobResult;
+
+fn inner_glob(pattern: &str, text: &str, mut p_idx: usize, mut t_idx: usize) -> GlobResult {
+    let p_chars: Vec<char> = pattern.chars().collect();
+    let t_chars: Vec<char> = text.chars().collect();
+
+    while p_idx < p_chars.len() && t_idx < t_chars.len() {
+        match p_chars[p_idx] {
+            '?' => {
+                p_idx += 1;
+                t_idx += 1;
+            }
+            '*' => {
+                let res: GlobResult = inner_glob(pattern, text, p_idx + 1, t_idx);
+                if res as i32 > 0 {
+                    return res;
+                }
+                t_idx += 1;
+            }
+            '[' => {
+                p_idx += 1; // skip '['
+                let mut is_match = false;
+
+                if p_idx >= p_chars.len() {
+                    return GlobResult::SyntaxError;
+                }
+
+                is_match |= p_chars[p_idx] == t_chars[t_idx];
+                p_idx += 1;
+
+                while p_idx < p_chars.len() && p_chars[p_idx] != ']' {
+                    match p_chars[p_idx] {
+                        '-' => {
+                            unimplemented!("- not implemented")
+                        }
+                        _ => {
+                            is_match |= p_chars[p_idx] == t_chars[t_idx];
+                            p_idx += 1;
+                        }
+                    }
+                }
+                if p_idx >= p_chars.len() || p_chars[p_idx] != ']' {
+                    return GlobResult::SyntaxError;
+                }
+                if !is_match {
+                    return GlobResult::Unmatched;
+                }
+                p_idx += 1;
+                t_idx += 1;
+            }
+            _ => {
+                if p_chars[p_idx] == t_chars[t_idx] {
+                    p_idx += 1;
+                    t_idx += 1;
+                } else {
+                    return GlobResult::Unmatched;
+                }
+            }
+        }
+    }
+    if t_idx >= t_chars.len() {
+        while let Some('*') = p_chars.get(p_idx) {
+            p_idx += 1;
+        }
+        return (p_idx >= p_chars.len()).into();
+    }
+    GlobResult::Unmatched
+}
+
+pub fn glob(pattern: &str, text: &str) -> GlobResult {
+    inner_glob(pattern, text, 0, 0)
+}
+
+#[test]
+fn test_question_mark() {
+    assert_eq!(glob("main.?", "main.c"), GlobResult::Matched);
+    assert_eq!(glob("?", "a"), GlobResult::Matched);
+    assert_eq!(glob("?", "ab"), GlobResult::Unmatched);
+    assert_eq!(glob("?", ""), GlobResult::Unmatched);
+}
+
+#[test]
+fn test_star() {
+    assert_eq!(glob("*", "a"), GlobResult::Matched);
+    assert_eq!(glob("*", "ab"), GlobResult::Matched);
+    assert_eq!(glob("*", ""), GlobResult::Matched);
+    assert_eq!(glob("*.c", "main.c"), GlobResult::Matched);
+    assert_eq!(glob("*", "main.c"), GlobResult::Matched);
+    assert_eq!(glob("*Law*", "LaLawyer"), GlobResult::Matched);
+    assert_eq!(glob("*Law*", "GrokLaw"), GlobResult::Matched);
+    assert_eq!(glob("*Law*", "Laws"), GlobResult::Matched);
+}
+
+#[test]
+fn test_bracket() {
+    assert_eq!(glob("[abc]", "a"), GlobResult::Matched);
+    assert_eq!(glob("[abc]", "b"), GlobResult::Matched);
+    assert_eq!(glob("[abc]", "c"), GlobResult::Matched);
+    assert_eq!(glob("[abc]", "d"), GlobResult::Unmatched);
+    assert_eq!(glob("[CB]at", "Cat"), GlobResult::Matched);
+    assert_eq!(glob("[CB]at", "Bat"), GlobResult::Matched);
+    assert_eq!(glob("[CB]at", "cat"), GlobResult::Unmatched);
+    assert_eq!(glob("[CB]at", "bat"), GlobResult::Unmatched);
+    assert_eq!(glob("[CB]at", "CBat"), GlobResult::Unmatched);
+    assert_eq!(glob("[][!]", "]"), GlobResult::Matched);
+    assert_eq!(glob("[][!]", "["), GlobResult::Matched);
+    assert_eq!(glob("[][!]", "!"), GlobResult::Matched);
+    assert_eq!(glob("[][!]", "a"), GlobResult::Unmatched);
+}
+
+#[test]
+fn test_syntax_error() {
+    assert_eq!(glob("*.[abc", "main.a"), GlobResult::SyntaxError);
+}
+
+#[test]
+fn test_glob() {
+    assert_eq!(glob("*.[abc]", "main.a"), GlobResult::Matched);
+    assert_eq!(glob("*.[abc]", "main.b"), GlobResult::Matched);
+    assert_eq!(glob("*.[abc]", "main.c"), GlobResult::Matched);
+    assert_eq!(glob("*.[abc]", "main.d"), GlobResult::Unmatched);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,74 +1,13 @@
-use std::process::exit;
+mod glob;
+mod glob_result;
 
-fn inner_glob(pattern: &str, text: &str, mut p_idx: usize, mut t_idx: usize) -> bool {
-    let p_chars: Vec<char> = pattern.chars().collect();
-    let t_chars: Vec<char> = text.chars().collect();
-
-    while p_idx < p_chars.len() && t_idx < t_chars.len() {
-        match p_chars[p_idx] {
-            '?' => {
-                p_idx += 1;
-                t_idx += 1;
-            }
-            '*' => {
-                if inner_glob(pattern, text, p_idx + 1, t_idx) {
-                    return true;
-                }
-                t_idx += 1;
-            }
-            '[' => {
-                p_idx += 1;
-                while p_chars[p_idx] != ']' {
-                    if p_chars[p_idx] == t_chars[t_idx] {
-                        p_idx += 1;
-                        t_idx += 1;
-                        break;
-                    }
-                    p_idx += 1;
-                }
-            },
-            _ => {
-                if p_chars[p_idx] != t_chars[t_idx] {
-                    return false;
-                }
-                p_idx += 1;
-                t_idx += 1;
-            }
-        }
-    }
-    if t_idx >= t_chars.len() {
-        while let Some('*') = p_chars.get(p_idx) {
-            p_idx += 1;
-        }
-        return p_idx >= p_chars.len();
-    }
-    false
-}
-
-fn glob(pattern: &str, text: &str) -> bool {
-    inner_glob(pattern, text, 0, 0)
-}
-
-fn check_glob(pattern: &str, text: &str, expected: bool) {
-    let out: bool = glob(pattern, text);
-    println!("{:>15} <=> {:15} = {}", pattern, text, out);
-    if out != expected {
-        eprintln!("FAILURE! Expected {}", expected);
-        exit(1);
-    }
-}
+use glob::glob;
 
 fn main() {
-    check_glob("main.?", "main.c", true);
-    println!();
-    check_glob("*.c", "main.c", true);
-    check_glob("*", "main.c", true);
-    check_glob("*Law*", "LaLawyer", true);
-    check_glob("*Law*", "GrokLaw", true);
-    check_glob("*Law*", "Laws", true);
-    println!();
-    check_glob("*.[abc]", "main.a", true);
-    check_glob("*.[abc]", "main.b", true);
-    check_glob("*.[abc]", "main.c", true);
-    check_glob("*.[abc]", "main.d", false);
+    println!("{}", glob("*.[abc", "main.a"));
 }
+
+// *.[abc      main.a
+// ^           ^
+// ^          ^
+// ^          ^
